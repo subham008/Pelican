d534215c5509a85b1d5ab4aa653f2f4f52b7e142
subham008 <shaw.subham14.05.2002@gmail.com>
2024-03-08 10:28:21 +0530

    rust configuration

---
+++ Pelican/target/debug/deps/Pelican-5bfab8bf7429fcdb.d
@@ -0,0 +1,5 @@
+F:\Rust_pro\Pelican\Pelican\target\debug\deps\libPelican-5bfab8bf7429fcdb.rmeta: src\main.rs
+
+F:\Rust_pro\Pelican\Pelican\target\debug\deps\Pelican-5bfab8bf7429fcdb.d: src\main.rs
+
+src\main.rs:
