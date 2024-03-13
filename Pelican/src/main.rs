d64dfe75f1f69746e64a80249263f12453fe7a5d
subham008 <shaw.subham14.05.2002@gmail.com>
2024-03-13 21:53:54 +0530

    identity file added

--- Pelican/src/main.rs
+++ Pelican/src/main.rs
@@ -1,4 +1,46 @@
+mod Identity;
 
-fn main() {
+use std::fmt::Display;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Read, Write};
+use std::path::Path;
 
+mod identity;
+
+fn createFile(name:String , data:String) -> File{
+    let path = Path::new(name.as_str());
+    let display = path.display();
+
+    // Open the file in write mode (creates a new file if it doesn't exist)
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("Couldn't create {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+
+    match file.write_all(data.as_bytes()) {
+        Err(why) => panic!("Couldn't write to {}: ", why),
+        Ok(_) => println!("Successfully wrote data to "),
+    }
+   return file;
+}
+
+
+fn main()  -> std::io::Result<()>{
+
+    let obtained:Vec<i64>=vec![78 , 90, 34];
+
+    let manager =identity::IdentityManager::new( obtained );
+
+
+    println!("new id {}" , manager.getNewID());
+    println!("new id {}" , manager.getNewID());
+    println!("new id {}" , manager.getNewID());
+    println!("new id {}" , manager.getNewID());
+    println!("new id {}" , manager.getNewID());
+    println!("new id {}" , manager.getNewID());
+    println!("new id {}" , manager.getNewID());
+    println!("new id {}" , manager.getNewID());
+
+    Ok(())
 }
--- Pelican/target/debug/deps/Pelican.d
+++ Pelican/target/debug/deps/Pelican.d
@@ -1,5 +1,7 @@
-F:\Rust_pro\Pelican\Pelican\target\debug\deps\Pelican.exe: src\main.rs
+F:\Rust_pro\Pelican\Pelican\target\debug\deps\Pelican.exe: src\main.rs src\Identity.rs src\identity.rs
 
-F:\Rust_pro\Pelican\Pelican\target\debug\deps\Pelican.d: src\main.rs
+F:\Rust_pro\Pelican\Pelican\target\debug\deps\Pelican.d: src\main.rs src\Identity.rs src\identity.rs
 
 src\main.rs:
+src\Identity.rs:
+src\identity.rs:
